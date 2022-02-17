2735b14b70ddb0b9d6df103920c659274257f137
riggben <riggben@gmail.com>
2022-02-16 20:19:11 -0500

    Start menu completed

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+
+    public Image muteIcon, unMuteIcon;
+    public string levelToLoad;
+
+    public bool muted = false;
+
+    public void ToggleMute()
+    {
+        muteIcon.gameObject.SetActive(!muteIcon.gameObject.activeSelf);
+        unMuteIcon.gameObject.SetActive(!muteIcon.gameObject.activeSelf);
+    }
+
+    public void Play()
+    {
+        SceneManager.LoadScene(levelToLoad);
+    }
+}
